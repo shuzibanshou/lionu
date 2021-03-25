72ebf3dfc245b8ceac4f9e692838071fe927f088
Peter Hua <diskrooms@gmail.com>
2021-03-25 22:39:56 +0800

    exception_catch

--- public/index.php
+++ public/index.php
@@ -67,4 +67,8 @@ $app = require rtrim($paths->systemDirectory, '/ ') . '/bootstrap.php';
  * Now that everything is setup, it's time to actually fire
  * up the engines and make this app do its thang.
  */
-$app->run();
+try {
+    $app->run();
+} catch(\Exception $e){
+    echo $e->getMessage();
+}
