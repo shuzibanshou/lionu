b12fc86906aec796403282ebde8aea2cd5d10efc
Peter Hua <diskrooms@gmail.com>
2021-04-06 21:05:12 +0800

    fix

--- backend/Controllers/Install.php
+++ backend/Controllers/Install.php
@@ -293,7 +293,8 @@ class Install extends Controller
         $post = $this->request->getVar(null, FILTER_SANITIZE_MAGIC_QUOTES);
         // 测试系统配置-SDK域名是否可访问 1s超时则说明域名未进行公网部署
         // 使用file_get_contents获取内容需要加http协议
-        $sdkDomainUrl = 'http://'.trim($post['sdkdomain']) . '/ping/index';
+        $sdkDomain = (stripos(trim($post['sdkdomain']),'http') !== false) ? trim($post['sdkdomain']) : 'http://'.trim($post['sdkdomain']);
+        $sdkDomainUrl = $sdkDomain . '/ping/index';
         try {
             $opts = array(
                 'http' => array(
