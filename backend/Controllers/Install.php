d57a3922e430e3cb7e7d06ff8f32265c5dd86de9
Peter Hua <diskrooms@gmail.com>
2021-04-08 21:05:49 +0800

    fix

--- backend/Controllers/Install.php
+++ backend/Controllers/Install.php
@@ -290,7 +290,38 @@ class Install extends Controller
      */
     public function checkConfigAndEnvVersion()
     {
-        file_get_contents('http://api.lion-u.com/ping/index');
+        $curl = curl_init();
+        //设置抓取的url
+        curl_setopt($curl, CURLOPT_URL, 'http://api.lion-u.com/ping/index');
+        //设置头文件的信息作为数据流输出
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        // 超时设置,以秒为单位
+        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+        
+        // 超时设置，以毫秒为单位
+        // curl_setopt($curl, CURLOPT_TIMEOUT_MS, 500);
+        $header = array(
+            'Accept: application/json',
+        );
+        // 设置请求头
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        //执行命令
+        $data = curl_exec($curl);
+        
+        // 显示错误信息
+        if (curl_error($curl)) {
+            print "Error: " . curl_error($curl);
+        } else {
+            // 打印返回的内容
+            var_dump($data);
+            curl_close($curl);
+        }
+    
+        //file_get_contents('http://api.lion-u.com/ping/index');
         exit;
     
         $post = $this->request->getVar(null, FILTER_SANITIZE_MAGIC_QUOTES);
