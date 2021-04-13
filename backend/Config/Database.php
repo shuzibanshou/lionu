b83fd6207920c343fd22605b48db84849fc0039c
51tui.vip <51tui.vip@gmail.com>
2021-04-13 22:40:45 +0800

    add_shell_execute_permisson

--- backend/Config/Database.php
+++ backend/Config/Database.php
@@ -1,9 +1,9 @@
 <?php namespace Config;
 
-define( 'hostname', '127.0.0.1' );
+define( 'hostname', '59.110.142.191' );
 define( 'username', 'root' );
-define( 'password', '' );
-define( 'database', 'lion-u' );
+define( 'password', 'QWERasdf1234' );
+define( 'database', 'lion-test' );
 define( 'port', '3306' );
 
 /**
