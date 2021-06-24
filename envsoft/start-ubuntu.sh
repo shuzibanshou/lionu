c03a5a89e1981671273ce807cae80e6f7b4097d2
root <root@localhost.localdomain>
2021-06-24 14:43:21 +0800

    dontneedpwdspark

--- envsoft/start-ubuntu.sh
+++ envsoft/start-ubuntu.sh
@@ -176,4 +176,22 @@ then
 exit 3
 fi
 
+##配置密钥并追加公钥以便 Spark 免密启动
+if [ ! -f ~/.ssh/id_rsa.spark ]
+then
+	ssh-keygen -t rsa -f ~/.ssh/id_rsa.spark -q -N ''
+	if [ -f ~/.ssh/id_rsa.spark.pub ]
+	then 
+		cat ~/.ssh/id_rsa.spark.pub >> ~/.ssh/authorized_keys
+		##追加config配置文件 如果config文件不存在 会自动新建
+		echo "\nHost localhost\nIdentityFile ~/.ssh/id_rsa.spark\nUser root\n"	>> ~/.ssh/config
+	else
+		echo '公钥文件不存在'
+		exit 11
+	fi
+else
+	echo '密钥文件已存在'
+	exit 10
+fi
+
 ./spark-2.4.7-bin-hadoop2.7/sbin/start-all.sh
