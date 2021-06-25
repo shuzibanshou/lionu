1995cd54ac10591fa315cd6aeb1d5bd975ec068e
root <root@localhost.localdomain>
2021-06-25 11:58:53 +0800

    fix

--- envsoft/get_linux_release.sh
+++ envsoft/get_linux_release.sh
@@ -2,14 +2,17 @@
 #检测linux发行版
 
 pkg=""
-if [[ $(cat /proc/version | grep -i "Red Hat") != "" ]]
-then
-	pkg="yum"
-elif [[ $(cat /proc/version | grep -i "Ubuntu" ) != ""  ||  $(cat /proc/version | grep -i "Debian" ) != "" ]]
-then
-	pkg="apt"
-elif [[ $(cat /proc/version | grep -i "SUSE") != "" ]]
-then
-	pkg="zypper"
-fi
+get_release_pkg(){
+	if [[ $(cat /proc/version | grep -i "Red Hat") != "" ]]
+	then
+		pkg="yum"
+	elif [[ $(cat /proc/version | grep -i "Ubuntu" ) != ""  ||  $(cat /proc/version | grep -i "Debian" ) != "" ]]
+	then
+		pkg="apt"
+	elif [[ $(cat /proc/version | grep -i "SUSE") != "" ]]
+	then
+		pkg="zypper"
+	fi
+}
+get_release_pkg
 echo ${pkg}
--- envsoft/start-ubuntu.sh
+++ envsoft/start-ubuntu.sh
@@ -195,3 +195,20 @@ else
 fi
 
 ./spark-2.4.7-bin-hadoop2.7/sbin/start-all.sh
+
+
+#########获取Linux发行版包管理器的函数
+get_release_pkg(){
+	pkg=""
+	if [[ $(cat /proc/version | grep -i "Red Hat") != "" ]]
+	then
+		pkg="yum"
+	elif [[ $(cat /proc/version | grep -i "Ubuntu" ) != ""  ||  $(cat /proc/version | grep -i "Debian" ) != "" ]]
+	then
+		pkg="apt"
+	elif [[ $(cat /proc/version | grep -i "SUSE") != "" ]]
+	then
+		pkg="zypper"
+	fi
+	return ${pkg}
+}
