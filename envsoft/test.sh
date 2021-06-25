c006e9228324ac5b91646f9e6739c623563529c1
root <root@localhost.localdomain>
2021-06-25 16:45:25 +0800

    fix

--- envsoft/test.sh
+++ envsoft/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if `service --status-all | grep 'php${php_version}-fpm'` | wl -gt 0
+if `service --status-all | grep 'php${php_version}-fpm' | wc -l` -gt 0
 then
 	echo 'abc'
 	service php${php_version}-fpm restart
