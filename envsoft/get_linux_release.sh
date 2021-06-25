22b1cf320062a2bb740020365300c8f94f1b4e54
root <root@localhost.localdomain>
2021-06-25 11:15:36 +0800

    add

---
+++ envsoft/get_linux_release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#检测linux发行版
+
+pkg=""
+if [[ $(cat /proc/version | grep -i "Red Hat") != "" ]]
+then
+	pkg="yum"
+elif [[ $(cat /proc/version | grep -i "Ubuntu" ) != ""  ||  $(cat /proc/version | grep -i "Debian" ) != "" ]]
+then
+	pkg="apt"
+elif [[ $(cat /proc/version | grep -i "SUSE") != "" ]]
+then
+	pkg="zypper"
+fi
+echo ${pkg}
