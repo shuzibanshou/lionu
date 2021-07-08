147e426ed91fb8f6666ceac3bf1b8e185dc7a240
root <root@localhost.localdomain>
2021-07-08 15:34:19 +0800

    fix

--- frontend/Controllers/Receive.php
+++ frontend/Controllers/Receive.php
@@ -212,7 +212,7 @@ class Receive extends BaseController
 	    $rk->setLogLevel(LOG_DEBUG);
 	    $rk->addBrokers('127.0.0.1:9092');
 	    $topic = $rk->newTopic('launch', $topicConf);
-	     echo json_encode($deviceLaunchData);
+	    //echo json_encode($deviceLaunchData);
 	    $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($deviceLaunchData));
 	    
 	    $len = $rk->getOutQLen();
